f083aae0a6839d155cb2ed44f46fa16d96cbca1e
chenxiaoqi_i <chenxiaoqi_i@didiglobal.com>
2021-02-24 23:39:37 +0800

    初始化脚手架

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+const App: FC = () => (
+  <div className="App">
+    <Button type="primary">购物网站</Button>
+  </div>
+);
+
+export default App;
