32d378f5d426724f85e373c9ee71ad1606ace829
1093919186 <1093919186@qq.com>
2021-03-01 17:08:26 +0800

    dev分支第一次提交

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,21 +1,20 @@
 // 使用react路由懒加载需要引入Suspense
 import React, { Suspense } from 'react'
-import { HashRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Loading from '../components/Loading'
 
 const Login = React.lazy(() => import(/*webpackChunkName:"Login" */ '../pages/Login/index'))
 const NotFound = React.lazy(() => import(/*webpackChunkName:"NotFound" */ '../components/NotFound'))
 
 const App: React.FC = function () {
-  return <HashRouter>
+  return <BrowserRouter>
     <Suspense fallback={<Loading/>}>
-      <Redirect path='/' exact to='/index'></Redirect>
       <Switch>
         <Route path='/login' exact component={Login}></Route>
         <Route path='/*' component={NotFound}></Route>
       </Switch>
     </Suspense>
-  </HashRouter>
+  </BrowserRouter>
 }
 
 export default App
